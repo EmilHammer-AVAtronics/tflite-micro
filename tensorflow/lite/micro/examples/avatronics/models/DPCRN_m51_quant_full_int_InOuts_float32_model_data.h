c8a6a3c5e54ed07c9e2e67f0dcf430c29a6ef4cd
Emil Hammer <emil.hammer@avatronics.com>
2024-05-24 11:44:22 +0200

    Generated files from Make

---
+++ tensorflow/lite/micro/examples/avatronics/models/DPCRN_m51_quant_full_int_InOuts_float32_model_data.h
@@ -0,0 +1,4 @@
+#include <cstdint>
+
+constexpr unsigned int g_DPCRN_m51_quant_full_int_InOuts_float32_model_data_size = 166616;
+extern const unsigned char g_DPCRN_m51_quant_full_int_InOuts_float32_model_data[];
