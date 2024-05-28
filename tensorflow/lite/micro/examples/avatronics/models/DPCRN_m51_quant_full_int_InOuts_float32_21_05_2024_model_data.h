7f1c6ca72bebe2e4fb1a7303f2310adb1a73152d
Emil Hammer <emil.hammer@avatronics.com>
2024-05-28 13:45:33 +0200

    Deleted generated files from models folder

--- tensorflow/lite/micro/examples/avatronics/models/DPCRN_m51_quant_full_int_InOuts_float32_21_05_2024_model_data.h
+++
@@ -1,4 +0,0 @@
-#include <cstdint>
-
-constexpr unsigned int g_DPCRN_m51_quant_full_int_InOuts_float32_21_05_2024_model_data_size = 167936;
-extern const unsigned char g_DPCRN_m51_quant_full_int_InOuts_float32_21_05_2024_model_data[];
--- tensorflow/lite/micro/examples/avatronics/models/DPCRN_m51_quant_full_int_InOuts_float32_model_data.h
+++
@@ -1,4 +0,0 @@
-#include <cstdint>
-
-constexpr unsigned int g_DPCRN_m51_quant_full_int_InOuts_float32_model_data_size = 166616;
-extern const unsigned char g_DPCRN_m51_quant_full_int_InOuts_float32_model_data[];
