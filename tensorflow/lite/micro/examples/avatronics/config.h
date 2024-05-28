f2fe510cdc5a0fd181d909a877b80a2016759365
Emil Hammer <emil.hammer@avatronics.com>
2024-05-28 13:48:51 +0200

    FULL_INT_INOUTS_FLOAT32 option added

--- tensorflow/lite/micro/examples/avatronics/config.h
+++ tensorflow/lite/micro/examples/avatronics/config.h
@@ -8,17 +8,18 @@
 /*Enables time measurement - BUILD_TYPE should be 'release' for this to work 
  *  0 to disable.
  *  1 to enable for micro_interpreter_graph.cc
- *  2 to enable for Airoha_DPCRN_m34_quant_full_int.cc */ 
+ *  2 to enable for airoha_nn_model.cc */
 #define MEASURE_CYCLES_TAKEN 2
 
 /*Print the values of the intermediate tensors in the model
  * Set to 1 to enable, 0 to disable. */
-#define PRINT_INTERMEDIATE_TENSORS 0
+#define PRINT_INTERMEDIATE_TENSORS 1
 
 /*Enables must match model datatype
  * Set to 1 to enable, 0 to disable.
  * OBS. ONLY ONE AT A TIME*/ 
-#define MODEL_DATATYPE_DOUBLE 1
+#define MODEL_DATATYPE_DOUBLE 0
 #define MODEL_DATATYPE_INT32 0
+#define FULL_INT_INOUTS_FLOAT32 1
 
 #endif /* CONFIG_H */
