354c468abd21b6cf95ee03b7b80322e5d7c4be6b
Emil Hammer <emil.hammer@avatronics.com>
2024-05-28 13:46:03 +0200

    New model + dependencies, added to folder "models"

---
+++ tensorflow/lite/micro/examples/avatronics/models/DPCRN_m52_quant_full_int_InOuts_float32_28_5_2024_model_data.h
@@ -0,0 +1,4 @@
+#include <cstdint>
+
+constexpr unsigned int g_DPCRN_m52_quant_full_int_InOuts_float32_28_5_2024_model_data_size = 171160;
+extern const unsigned char g_DPCRN_m52_quant_full_int_InOuts_float32_28_5_2024_model_data[];
