3fcf8e18b5fff61c8a6ae394133baeb451cd3a25
Emil Hammer <emil.hammer@avatronics.com>
2024-06-12 09:28:28 +0200

    added model

---
+++ tensorflow/lite/micro/examples/avatronics/models/DPCRN_m53_quant_full_int_InOuts_float32_31_5_2024_model_data.h
@@ -0,0 +1,4 @@
+#include <cstdint>
+
+constexpr unsigned int g_DPCRN_m53_quant_full_int_InOuts_float32_31_5_2024_model_data_size = 143064;
+extern const unsigned char g_DPCRN_m53_quant_full_int_InOuts_float32_31_5_2024_model_data[];
